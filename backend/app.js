10ef0a5c990da0e78556d37e2513613cb530f072
kasettyvenkatasai <venkatsaikp7@gmail.com>
2024-12-17 12:43:58 +0530

    added backend files

---
+++ backend/app.js
@@ -0,0 +1,20 @@
+const express =  require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const app = express();
+const router = require('./routes/route');
+const cookieParser = require('cookie-parser');
+const port = 3020;
+const {connectMongoDb} = require('./connection/connection')
+app.use(cors({
+    origin:'http://localhost:3000',
+    credentials:true
+}))
+connectMongoDb('mongodb://localhost:27017/auth').then(()=>{console.log("mongodb connected successfully")}).catch((err)=>{console.log(err)});
+app.use(cookieParser());
+app.use(express.json()); 
+app.use(express.urlencoded({ extended: false })); 
+app.use(router);
+app.listen(port,()=>{
+    console.log("server is listening on port" + port);
+})
---
+++ backend/connection/connection.js
@@ -0,0 +1,5 @@
+const mongoose = require('mongoose');
+async function connectMongoDb(url) {
+    mongoose.connect(url);
+}
+module.exports = {connectMongoDb}
